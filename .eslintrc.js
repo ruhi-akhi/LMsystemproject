d9a2d9b4aecfb8e2e4d7f5a636510db07b694da3
akhiakter25556 <akhiakterherpower80@gmail.com>
2026-04-13 11:36:06 +0600

    fix: eslint config update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
+cat > .eslintrc.js << 'EOF'
 module.exports = {
   extends: "next/core-web-vitals",
   rules: {
     "@next/next/no-html-link-for-pages": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-explicit-any": "off",
   },
 };
+EOF
